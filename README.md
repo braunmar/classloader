a1feaa34ec94ef5047820fb1ef6643dd2433951d
Maraw3 <marek.braun@seznam.cz>
2016-02-26 11:29:13 +0100

    Update LICENCE & README.md

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 Class loader
 ==============================
+Class robot loader for find and require classes.
+
+Instalation
+-----------
+
+
+Documentation
+-------------
+For full documentation see this link at [demos.braunmar.cz/classloader](http://demos.braunmar.cz/classloader).
+
+Licence
+-------
+Class robot loader is under MIT licence. See the [LICENCE.md](https://github.com/braunmar/classloader/blob/master/LICENSE.md) for details.
--- src/ClassLoader.php
+++ src/ClassLoader.php
@@ -7,7 +7,7 @@
  * file that was distributed with this source code.
  */
 
-namespace braunmar\simple\classloader; 
+namespace braunmar\simple\classloader;
 
 /**
  * Class robot loader.
@@ -25,7 +25,8 @@ namespace braunmar\simple\classloader;
  * See https://github.com/braunmar/simplecache
  */
 class ClassLoader
-{    
+{
+
     /**
      * All accept extension of file
      * @var array
@@ -37,31 +38,31 @@ class ClassLoader
      * @var $array
      */
     protected $classes = [];
-    
+
     /**
      * Ignored dirs
      * @var array
      */
     protected $ignoreDirs = [];
-    
+
     /**
      * Dirs where loader try find classes
      * @var type 
      */
     protected $dirs = [];
-    
+
     /**
      * Instance of cache (must implement method cache and load)
      * @var type 
      */
     private $cache = false;
-        
+
     /**
      * Static instance
      * @var ClassLoader
      */
     public static $instance;
-    
+
     /**
      * Static constructor
      * @return ClassLoader
@@ -71,19 +72,19 @@ class ClassLoader
         if (self::$instance === null) {
             self::$instance = new static;
         }
-        
+
         return self::$instance;
     }
-    
+
     /**
      * Register (enable) class loader
      * @param boolean $prepend sql_autoalod_register prepend parameter
      */
-    public function register($prepend = false) 
-    {       
+    public function register($prepend = false)
+    {
         spl_autoload_register([$this, 'tryLoad'], true, $prepend);
     }
-    
+
     /**
      * Unregister (disable) class loader
      */
@@ -91,7 +92,7 @@ class ClassLoader
     {
         spl_autoload_unregister([$this, 'tryLoad']);
     }
-    
+
     /**
      * Add dir where class loader try find classes (works recursively)
      * @param string $dir Full dir path
@@ -100,22 +101,22 @@ class ClassLoader
     public function addDir($dir)
     {
         $this->dirs[] = $dir;
-        
+
         return $this;
     }
-    
+
     /**
      * Add dirs where class loader try find classes (works recursively)
-     * @param array $dirs Array of ull dirpath where class loader try find classes
+     * @param array $dirs Array of full dirpath where class loader try find classes
      * @return ClassLoader $this
      */
     public function addDirs(array $dirs)
     {
         $this->dirs = array_merge($this->dirs, $dirs);
-        
+
         return $this;
     }
-    
+
     /**
      * Add ignore dir. Dir is ignored when class loader try find classes
      * @param string $dir Full dir path
@@ -124,10 +125,10 @@ class ClassLoader
     public function addIgnoreDir($dir)
     {
         $this->ignoreDirs[] = $dir;
-        
+
         return $this;
     }
-    
+
     /**
      * Add ignore dirs. Dirs is ignored when class loader try find classes
      * @param array $dirs Array of full dir path
@@ -136,7 +137,7 @@ class ClassLoader
     public function addIgnoreDirs(array $dirs)
     {
         $this->ignoreDirs = array_merge($this->ignoreDirs, $dirs);
-        
+
         return $this;
     }
 
@@ -152,46 +153,44 @@ class ClassLoader
         if (!is_object($cache)) {
             throw new \InvalidArgumentException("Cache must by a object");
         }
-        
+
         /* You don't wanna use Interface for this */
         if (!method_exists($cache, 'load') || !method_exists($cache, 'cache')) {
             throw new \InvalidArgumentException('Cache object must implement methods "load()" and "cache($data)');
         }
-        
+
         $this->cache = $cache;
-        
+
         if ($load) {
             $loadClasses = $this->cache->load();
-            
+
             if (is_array($loadClasses)) {
                 $this->classes = array_merge($this->classes, $loadClasses);
-            }
-            
-            else if ($loadClasses instanceof \stdClass) {
+            } else if ($loadClasses instanceof \stdClass) {
                 $loadClasses = $this->classes = array_merge($this->classes, (array) $loadClasses);
             }
         }
-        
+
         return $this;
     }
-    
+
     /**
      * Try load class
      * @param sting $type Full classname
      */
     protected function tryLoad($type)
     {
-       /* Robot load */
-       if (($path = $this->findClass($type)) !== false) {
-           
+        /* Robot load */
+        if (($path = $this->findClass($type)) !== false) {
+
             requireFile($path);
-           
+
             if ($this->cache) {
                 $this->cache->cache($this->classes);
             }
-       }
+        }
     }
-    
+
     /**
      * Find class
      * @param string $class Classname
@@ -200,26 +199,26 @@ class ClassLoader
     protected function findClass($class)
     {
         $class = ltrim($class, '\\');
-        
+
         if (key_exists($class, $this->classes)) {
-            return $this->classes[$class];      
+            return $this->classes[$class];
         }
-        
-        if(is_file($file = "\\" . $class .'.php')) {
+
+        if (is_file($file = "\\" . $class . '.php')) {
             $this->classes[$class] = $file;
             return $file;
         }
-        
-        $split = explode("\\", $class);                
+
+        $split = explode("\\", $class);
         foreach ($this->dirs as $dir) {
-            if(($file = $this->searchFor(end($split), $dir, $class)) !== false) {
+            if (($file = $this->searchFor(end($split), $dir, $class)) !== false) {
                 return $file;
             }
         }
-        
+
         return false;
     }
-    
+
     /**
      * Find class in dirs
      * @param string $class Classname
@@ -227,15 +226,15 @@ class ClassLoader
      * @param string $type Full classname (with namespaces)
      * @return string|boolean False if not finded. Full path to file if finded
      */
-    protected function searchFor($class, $in, $type) 
+    protected function searchFor($class, $in, $type)
     {
         $handle = opendir($in);
-        
+
         if ($handle == false) {
             closedir($handle);
-            return false; 
+            return false;
         }
-        
+
         while (false !== ($entry = readdir($handle))) {
             if ($entry === '.' || $entry === '..') {
                 continue;
@@ -256,27 +255,28 @@ class ClassLoader
                 return $path;
             }
         }
-        
+
         closedir($handle);
 
         return false;
     }
-    
+
     /**
      * Get filename and check filename extension
      * @param string $filename
      * @return string Filename (withnout extension)
      */
-   protected function getFilename($filename)
+    protected function getFilename($filename)
     {
-       $extension = pathinfo($filename, PATHINFO_EXTENSION);
-       
-       if (!$extension || !in_array($extension, $this->acceptFile)) {
-           return null;
-       }
-       
-       return pathinfo($filename, PATHINFO_FILENAME);
+        $extension = pathinfo($filename, PATHINFO_EXTENSION);
+
+        if (!$extension || !in_array($extension, $this->acceptFile)) {
+            return null;
+        }
+
+        return pathinfo($filename, PATHINFO_FILENAME);
     }
+
 }
 
 /**
